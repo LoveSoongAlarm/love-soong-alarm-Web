3c720bb2a4e689561504e641ab46bb7ed81d1a56
승민 <seungmin@seungmin-ui-MacBookPro.local>
2025-09-22 03:01:10 +0900

    feat: 외부 링크 연동
    feat: #41

--- src/pages/Setting.tsx
+++ src/pages/Setting.tsx
@@ -78,12 +78,17 @@ export const Setting = () => {
           <div className="w-full h-1 bg-divider-regular my-2.5" />
 
           <List
-            onClick={() => navigate("/")}
+            onClick={() =>
+              (window.location.href = "http://pf.kakao.com/_xbxakDn/chat")
+            }
             img={Kakao}
             title="카카오톡 문의"
           />
           <List
-            onClick={() => navigate("/")}
+            onClick={() =>
+              (window.location.href =
+                "https://www.instagram.com/love_sonng_alarm?igsh=MXhreHFzcDQzZDUzbw%3D%3D&utm_source=qr")
+            }
             img={Insta}
             title="인스타그램 문의"
           />
